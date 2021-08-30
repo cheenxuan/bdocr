69ddd32fb4d9081f23d677b7c0d064b77f31479d
xuan <xuanqinjian@gmail.com>
2021-08-30 18:02:46 +0800

    modify sdk token parser

--- bdocr/src/main/java/me/xuan/bdocr/sdk/utils/AccessTokenParser.java
+++ bdocr/src/main/java/me/xuan/bdocr/sdk/utils/AccessTokenParser.java
@@ -19,51 +19,33 @@ public class AccessTokenParser implements Parser<AccessToken> {
         SDKError error;
         try {
             JSONObject jsonObject = new JSONObject(json);
-            if(!jsonObject.isNull("access_token")) {
-                AccessToken accessToken = new AccessToken();
-                accessToken.setTokenJson(json);
-                accessToken.setAccessToken(jsonObject.getString("access_token"));
-                if(jsonObject.has("expires_in")) {
-                    accessToken.setExpiresIn(jsonObject.getInt("expires_in"));
-                }
-                return accessToken;
-            }else{
-                if(!jsonObject.isNull("error")){
-                    error = new SDKError(SDKError.ErrorCode.ACCESS_TOKEN_DATA_ERROR, "Server illegal response " + jsonObject.getString("error_description"));
-                    throw error;
-                }else{
-                    error = new SDKError(SDKError.ErrorCode.ACCESS_TOKEN_DATA_ERROR, "Server illegal response " + json);
-                    throw error;
-                }
-            }
+            if (!jsonObject.isNull("status")) {
+                int status = jsonObject.optInt("status");
+                if (status == 0) {
+                    JSONObject data = jsonObject.optJSONObject("data");
+                    if (data != null) {
+                        AccessToken accessToken = new AccessToken();
+                        accessToken.setTokenJson(json);
+                        accessToken.setAccessToken(data.getString("access_token"));
+                        if (data.has("lic")) {
+                            accessToken.setLic(data.getString("lic"));
+                        }
 
-            //            if(!jsonObject.isNull("status")) {
-            //                int status = jsonObject.optInt("status");
-            //                if(status == 0) {
-            //
-            //                    if(data != null) {
-            //                        AccessToken accessToken = new AccessToken();
-            //                        accessToken.setTokenJson(json);
-            //                        accessToken.setAccessToken(data.getString("access_token"));
-            //                        if(data.has("lic")) {
-            //                            accessToken.setLic(data.getString("lic"));
-            //                        }
-            //
-            //                        accessToken.setExpiresIn(data.optInt("expires_in"));
-            //                        return accessToken;
-            //                    } else {
-            //                        return null;
-            //                    }
-            //                } else {
-            //                    String message = jsonObject.optString("message");
-            //                    Long logId = jsonObject.optLong("log_id");
-            //                    error = new SDKError(status, message + " logId: " + logId);
-            //                    throw error;
-            //                }
-            //            } else {
-            //                error = new SDKError(283505, "Server illegal response " + json);
-            //                throw error;
-            //            }
+                        accessToken.setExpiresIn(data.optInt("expires_in"));
+                        return accessToken;
+                    } else {
+                        return null;
+                    }
+                } else {
+                    String message = jsonObject.optString("message");
+                    Long logId = jsonObject.optLong("log_id");
+                    error = new SDKError(status, message + " logId: " + logId);
+                    throw error;
+                }
+            } else {
+                error = new SDKError(SDKError.ErrorCode.ACCESS_TOKEN_DATA_ERROR, "Server illegal response " + json);
+                throw error;
+            }
         } catch (JSONException var7) {
             error = new SDKError(SDKError.ErrorCode.ACCESS_TOKEN_DATA_ERROR, "Server illegal response " + json, var7);
             throw error;
